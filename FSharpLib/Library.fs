e71c8bea4477b9badc1b793d5959b26f6777d84c
vadim.korochkin <vadim.korochkin@secretcircle.solutions>
2024-02-20 15:14:40 +0600

    wip

--- FSharpLib/Library.fs
+++ FSharpLib/Library.fs
@@ -2,14 +2,15 @@
 
 open CSharpLib
 
-module Say =
-    let entity = Entity(1)
-    
-    // in Rider 2023.3.3, it shows 'set_Id' as a member of 'Entity' type
-    // but should show "Id" property, which even doesn't have a setter actually
-    entity.set_Id(1)
-    
-    // VisualStudio and Rider 2022.3.3 show "Id" property as expected
-    //entity.Id
-    
+module Test =
+    let DemonstrateIssue() =
+        let entity = Entity(1)
+        
+        // in Rider 2023.3.3, it shows 'set_Id' as a member of 'Entity' type
+        // but should show "Id" property, which even doesn't have a setter actually
+        entity.set_Id(1)
+        
+        // VisualStudio and Rider 2022.3.3 show "Id" property as expected
+        //entity.Id
+        
     
